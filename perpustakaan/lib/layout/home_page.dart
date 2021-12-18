97a639e2b7905a86eff501464b9069b976a17389
Fanovv <affannovian@gmail.com>
2021-12-18 17:35:00 +0700

    Sambungin webservice Kategori sama List buku

--- perpustakaan/lib/layout/home_page.dart
+++ perpustakaan/lib/layout/home_page.dart
@@ -2,8 +2,10 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:flutter/material.dart';
 import 'package:perpustakaan/layout/listbook_page.dart';
 import 'package:perpustakaan/models/buku.dart';
+import 'package:perpustakaan/models/kategori.dart';
 import 'package:perpustakaan/server_handler.dart';
 import 'package:perpustakaan/widgets/home_buku_item.dart';
+import 'package:perpustakaan/widgets/home_kategori_item.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -16,6 +18,11 @@ class _HomePageState extends State<HomePage> {
   Future<List<Buku>> getBuku() async {
     List<Buku> buku = await ServerHandler().listBuku();
     return buku;
+  }
+
+  Future<List<Kategori>> getKategori() async {
+    List<Kategori> kategori = await ServerHandler().listKategori();
+    return kategori;
   }
 
   @override
@@ -46,9 +53,9 @@ class _HomePageState extends State<HomePage> {
         ),
         body: SingleChildScrollView(
             child: Padding(
-              padding: EdgeInsets.only(left: screenwidth * 0.02),
-              child: Column(
-          children: <Widget>[
+          padding: EdgeInsets.only(left: screenwidth * 0.02),
+          child: Column(
+            children: <Widget>[
               Padding(
                 padding: EdgeInsets.fromLTRB(
                     screenwidth * 0.03,
@@ -102,7 +109,7 @@ class _HomePageState extends State<HomePage> {
                     child: InkWell(
                       onTap: () {
                         Navigator.of(context).push(MaterialPageRoute(
-                          builder: (context) => ListBook()));
+                            builder: (context) => const ListBook()));
                       },
                       child: Text(
                         "SEE ALL",
@@ -122,8 +129,8 @@ class _HomePageState extends State<HomePage> {
                 height: screenheight * 0.35,
                 child: FutureBuilder<List<Buku>>(
                   future: getBuku(),
-                  builder:
-                      (BuildContext context, AsyncSnapshot<List<Buku>> snapshot) {
+                  builder: (BuildContext context,
+                      AsyncSnapshot<List<Buku>> snapshot) {
                     if (snapshot.hasData) {
                       return ListView(
                         scrollDirection: Axis.horizontal,
@@ -157,85 +164,25 @@ class _HomePageState extends State<HomePage> {
                 ],
               ),
               SizedBox(
-                height: screenheight * 0.35,
-                child: GridView.count(
-                  crossAxisCount: 2,
-                  children: <Widget>[
-                    Kategori(
-                        context: context,
-                        image: "lib/assets/buku_Bulan.png",
-                        title: "Programming",),
-                    Kategori(
-                        context: context,
-                        image: "lib/assets/buku_Bulan.png",
-                        title: "Drama",),
-                    Kategori(
-                        context: context,
-                        image: "lib/assets/buku_Bulan.png",
-                        title: "Drama",),
-                    Kategori(
-                        context: context,
-                        image: "lib/assets/buku_Bulan.png",
-                        title: "Drama",),
-                  ],
-                ),
-              )
-          ],
-        ),
-            )),
-      ),
-    );
-  }
-
-  Widget Kategori({required BuildContext context, image, title}) {
-    var screenwidth = MediaQuery.of(context).size.width;
-    var screenheight = MediaQuery.of(context).size.height;
-    return Padding(
-      padding:
-          EdgeInsets.fromLTRB(screenwidth * 0.04, 0, 0, screenheight * 0.005),
-      child: SizedBox(
-        width: screenwidth / 1 / 3,
-        height: screenheight /1 /3,
-        child: Card(
-          elevation: 0.0,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Container(
-                height: screenheight / 1 / 5.5,
-                width: screenwidth / 1 / 3,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
-                  image: DecorationImage(
-                    image: AssetImage(
-                      image,
-                    ),
-                    fit: BoxFit.cover,
-                  ),
-                ),
-              ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: <Widget>[
-                  Padding(
-                    padding: const EdgeInsets.only(top: 0.5),
-                    child: Text(
-                      title,
-                      style: GoogleFonts.ubuntu(
-                        textStyle: TextStyle(
-                            color: Colors.blue,
-                            letterSpacing: .2,
-                            fontSize: screenheight * 0.02,
-                            fontWeight: FontWeight.w500),
-                      ),
-                    ),
-                  ),
-                ],
-              )
+                  height: screenheight * 0.35,
+                  child: FutureBuilder<List<Kategori>>(
+                    future: getKategori(),
+                    builder: (context, snapshot) {
+                      if (snapshot.hasData) {
+                        return GridView.count(
+                          crossAxisCount: 2,
+                          children: snapshot.data!
+                              .map((e) => HomeKategoriItem(kategori: e))
+                              .toList(),
+                        );
+                      } else {
+                        return const CircularProgressIndicator();
+                      }
+                    },
+                  ))
             ],
           ),
-        ),
+        )),
       ),
     );
   }
